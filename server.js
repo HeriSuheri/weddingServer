c4e94d9f4b8a37ed53eefd5794c49b81b665bf84
Heri <herysuhery94@gmail.com>
2025-07-15 18:34:51 +0700

    fix: URI

--- server.js
+++ server.js
@@ -38,11 +38,11 @@ guestSchema.index({ attendance: 1 });
 guestSchema.index({ createdAt: -1 });
 
 const Guest = mongoose.model("Guest", guestSchema);
-console.log("🌱 Environment Key MONGO_URL ada?", 'MONGO_URL' in process.env);
-console.log("🌐 Nilai process.env.MONGO_URL:", process.env.MONGO_URL);
-console.log("🔍 ENV MONGO_URL:", process.env.MONGO_URL);
+console.log("🌱 Environment Key MONGO_URI ada?", 'MONGO_URI' in process.env);
+console.log("🌐 Nilai process.env.MONGO_URL:", process.env.MONGO_URI);
+console.log("🔍 ENV MONGO_URL:", process.env.MONGO_URI);
 
-mongoose.connect(process.env.MONGO_URL, {
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
